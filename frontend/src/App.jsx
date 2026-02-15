bb3570bf3334b90bb2656323ac9dde2c69de1864
the-narayani <narayanikk15@gmail.com>
2026-02-15 16:45:54 -0500

    render MovieCard component in App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,8 @@ import MovieCard from './components/MovieCard'
 function App() {
   return (
       <div>
-        
+        <MovieCard movie = {{title: "tim", release_date: 2024}}/>
+        <MovieCard movie = {{title: "joe", release_date: 2025}}/>
       </div>      
   )
 }
