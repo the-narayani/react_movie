e89c508cf0623a714d88a6197692d74c277a2c94
the-narayani <narayanikk15@gmail.com>
2026-02-15 22:28:48 -0500

    search bar in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,13 +8,20 @@ function Home(){
     
 ];
     
+const handleSearch = () => {
 
+};
 return(
         <div className="home">
-            
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                type ="text"
+                placeholder="Search for movies..."
+                className="search-input"/>
+            </form>
             <div className ="movies-grid">
                 {movies.map((movie) => (
-
+                    
                     <MovieCard movie = {movie} key={movie.id}/>
 
                 ))}
