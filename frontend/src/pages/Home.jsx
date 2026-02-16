d6ab00b86013e720545e5020f25fb7073ef0e503
the-narayani <narayanikk15@gmail.com>
2026-02-15 22:28:12 -0500

    rendered MovieCard component in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,20 +8,15 @@ function Home(){
     
 ];
     
-const handleSearch = () => {
 
-};
 return(
         <div className="home">
-            <form onSubmit={handleSearch} className="search-form">
-                <input
-                type ="text"
-                placeholder="Search for movies..."
-                className="search-input"/>
-            </form>
+            
             <div className ="movies-grid">
                 {movies.map((movie) => (
+
                     <MovieCard movie = {movie} key={movie.id}/>
+
                 ))}
             </div>
         </div>
